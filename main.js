d4733d591c50f8fd8bc342ed44d3be3ea19fb35f
garrettgraber <garrett_graber@yahoo.com>
2014-07-10 13:08:07 -0600

    Add, Delete, and Undo are working both programmatically and localStorage. Radio buttons added, but are shit.

--- main.js
+++ main.js
@@ -1,16 +1,44 @@
 
+// Global variables
 var localStoreName = 'quotes';
+var idQuoteBase = 'zz';
+localStorage.clear();
+var lastDelete;
 
-var QuoteObject = function(quoteText, quoteAuthor, quoteRating) {
+var randomNumber = function() {
+	var maxRanVal = 30000;
+	var minRanVal = 10000;
+	return Math.floor(Math.random() * (maxRanVal - minRanVal) + minRanVal);
+};
+
+var currentIdValue = randomNumber();
+console.log('currentIdValue: ' + currentIdValue);
+
+var QuoteObject = function(idValue, quoteText, quoteAuthor, quoteRating) {
 	this.quoteText = quoteText;
 	this.quoteAuthor = quoteAuthor;
 	this.quoteRating = quoteRating;
+	this.valID = idValue;
+	var that = this;
+	console.log(this.valID);
 	
 	this.render = function() {
 		console.log('Create DOM function has fired');
-		var tempJqueryObject = $('<div class="quote-container">' + this.quoteText + '<p class="quote-author">' + this.quoteAuthor + '</p><div class="quote-rating">' + this.averageValue + '</div></div>');
-		console.log(tempJqueryObject);
+		var tempJqueryObject = $('<div class="quote-container"><p class="quote-text">' + this.quoteText 
+			+ '</p><p class="quote-author"> - ' + this.quoteAuthor + '</p><div class="quote-rating">Rating:  ' 
+			+ this.averageValue + '</div><button class="delete-button">Delete</button>' 
+			+ '<div class="radio-container><p>Radio Buttons</p>'
+			+ '<input class="rate-button rate1" type="Radio"> 1'
+			+ '<input class="rate-button rate2" type="Radio"> 2'
+			+ '<input class="rate-button rate3" type="Radio"> 3'
+			+ '<input class="rate-button rate4" type="Radio"> 4'
+			+ '<input class="rate-button rate5" type="Radio"> 5'
+			+ '</div>'
+			+ '</div>');
+		console.log('tempJqueryObject: ' + tempJqueryObject);
+		tempJqueryObject = setElementID(tempJqueryObject, this.valID);
 		$('#main').prepend(tempJqueryObject);
+		console.log('render has finished');
 	};
 	this.store = function(inProp) {
 		var outputArray = createQuoteObjectArray(inProp);
@@ -20,7 +48,8 @@ var QuoteObject = function(quoteText, quoteAuthor, quoteRating) {
 	};
 	this.average = function() {
 		var sum = _.reduce(this.quoteRating, function(memo, num){ return memo + num; }, 0);
-		return (sum/(this.quoteRating.length)).toFixed(0);
+		console.log('that.quoteRating.length: ' + that.quoteRating.length);
+		return parseFloat( (sum/(that.quoteRating.length)).toFixed(0) );
 	};
 
 	this.averageValue = this.average();
@@ -37,16 +66,15 @@ var findObjectWithCertainValue = function(inArray, inProp, inId) {
 };
 
 var removeObjectFromArray = function(tempObjectIn, inArray) {
-	if( !(tempObject instanceof Array) ) {
-		var tempObjectIndex = inArray.indexOf(tempObject); 
+	if( !(tempObjectIn instanceof Array) ) {
+		var tempObjectIndex = inArray.indexOf(tempObjectIn); 
 		var removedObject = inArray.splice(tempObjectIndex, 1);
 		console.log('Object removed from list');
-		return true;
 	}
 	else {
 		console.log('You returned multiple objects with the same id');
-		return false;
 	}
+	return inArray;
 };
 
 var turnArrayJsonAndStore = function(inProp, inArray) {
@@ -65,7 +93,7 @@ var createObjectListWithMethods = function(inArray) {
 	var outputArray = [];
 	for(var i=0; i < inArray.length; i++) {
 		var tempObject = inArray[ i ];
-		var newObject = new QuoteObject('', '', '');
+		var newObject = new QuoteObject('', '', '', []);
 		for(key in tempObject) {
 			newObject[ key ] = tempObject[ key ];
 
@@ -75,23 +103,66 @@ var createObjectListWithMethods = function(inArray) {
 	return outputArray;
 };
 
+var createObjectWithMethods = function(tempObject) {
+	var newObject = new QuoteObject('', '', '', []);
+	for(key in tempObject) {
+		newObject[ key ] = tempObject[ key ];
+	}
+	return newObject;
+};
+
 var createQuoteObjectArray = function(inProp) {
 	return createObjectListWithMethods( storageToArray(inProp) );
 };
 
 var storeObjectLocalStorage = function(inProp, inObject) {
 	var outputArray = createQuoteObjectArray( inProp );
+	console.log('outputArray: ' + outputArray);
 	outputArray.unshift(inObject);
 	return turnArrayJsonAndStore(inProp, outputArray);
 };
 
-var c = [new QuoteObject('hello', 'GOD', [{id: 0}]), new QuoteObject('herroo', 'Chairman Meow', [4]), new QuoteObject('suck it', 'Me', [10,1])]
+var turnObjectValueToFloat = function(keyString, normalObject, checkString) {
+	var outputVal;
+	((keyString.slice(keyString.length - checkString.length)) === checkString) ? outputVal = parseFloat(normalObject[keyString]) : outputVal = normalObject[keyString];
+	return outputVal;
+};
 
+var checkValueAndKeySync = function(keyString, normalObject, checkString) {
+	var outputVal;
+	((keyString.slice(keyString.length - checkString.length)) === checkString) ? outputVal = true : outputVal = false;
+	return outputVal;
+};
 
+var setElementID = function(jqueryObject, currentObjectId) {
+	return jqueryObject.attr("id", currentObjectId);
+};
 
+var createNewId = function(typeId, currentIdTemp) {
+	var currentIdTemp = currentIdTemp + 10;
+	console.log('currentIdValue: ' + currentIdTemp);
+	var newId = typeId + currentIdTemp.toString();
+	console.log('newId: ' + newId);
+	currentIdValue = currentIdTemp;
+	return newId;
+};
 
+var c = [new QuoteObject(createNewId(idQuoteBase, currentIdValue), 'hello', 'GOD', [0,1,2,3,3]), new QuoteObject(createNewId(idQuoteBase, currentIdValue), 'herroo', 'Chairman Meow', [5,2,3]), new QuoteObject(createNewId(idQuoteBase, currentIdValue), 'suck it', 'Me', [ 5, 4 ] ) ];
 
+var appendListToMainContainer = function(inArray) {
+	for(var i=0; i < inArray.length; i++) {
+		var tempObject = inArray[ i ];
 
+		console.log('append list on: ' + i);
+		tempObject.render();
+
+		storeObjectLocalStorage(localStoreName, tempObject);
+		console.log('object has been stored');
+	}
+};
+
+// Input quotes
+appendListToMainContainer(c);
 
 $(document).on('ready', function() {
 
@@ -102,9 +173,36 @@ $(document).on('ready', function() {
 		console.log(tempTextValue);
 		console.log(tempAuthorValue);
 		alert('STOP!');
-		var tempQuoteObject = new QuoteObject(tempTextValue, tempAuthorValue, [0]);
-		tempQuoteObject.render();
+		var tempQuoteObject = new QuoteObject(createNewId(idQuoteBase, currentIdValue), tempTextValue, tempAuthorValue, [0]);
+		
 		storeObjectLocalStorage(localStoreName, tempQuoteObject);
+		tempQuoteObject.render();
+		
+	});
+
+	$('.undo-delete-button').click(function() {
+		if(lastDelete !== undefined) {
+			var tempObject = createObjectWithMethods(lastDelete);
+			tempObject.render();
+			storeObjectLocalStorage(localStoreName, tempObject);
+		}
+		else {
+			alert('No items have been deleted');
+		}
+	});
+
+	$(document).on('click', '.delete-button', function() {
+		var containerObject = $(this).closest('.quote-container');
+		console.log($(this));
+		var containerObjectID = containerObject.attr("id");
+		console.log('containerObjectID: ' + containerObjectID);
+		var quoteArray = storageToArray(localStoreName);
+
+		var foundObject = findObjectWithCertainValue(quoteArray, 'valID', containerObjectID)
+		lastDelete = foundObject;
+		quoteArray = removeObjectFromArray(foundObject, quoteArray);
+		turnArrayJsonAndStore(localStoreName, quoteArray);
+		containerObject.remove();
 	});
 
   
